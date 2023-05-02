5c70095e94aef6d8d5848fdbadc0f0074e424834
hauh6976 <131356933+hauh6976@users.noreply.github.com>
2023-05-03 01:14:59 +0300

    Create dex.sol

---
+++ dex.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
+contract TokenSwapper {
+    address public uniswapRouter;
+    address public tokenA;
+    address public tokenB;
+    
+    constructor(address _uniswapRouter, address _tokenA, address _tokenB) {
+        uniswapRouter = _uniswapRouter;
+        tokenA = _tokenA;
+        tokenB = _tokenB;
+    }
+
+    function swapTokens(uint amountIn, uint amountOutMin) public {
+        IERC20(tokenA).approve(uniswapRouter, amountIn);
+        address[] memory path = new address[](2);
+        path[0] = tokenA;
+        path[1] = tokenB;
+        uint deadline = block.timestamp + 3600;
+        IUniswapV2Router02(uniswapRouter).swapExactTokensForTokens(
+            amountIn,
+            amountOutMin,
+            path,
+            msg.sender,
+            deadline
+        );
+    }
+}
